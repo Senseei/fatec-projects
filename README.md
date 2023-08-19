453389122aee828136268f9bb2778ed11c06dacb
Gabriel de Jesus <gabrieldjesus475@gmail.com>
2023-08-19 16:47:54 -0300

    Numeric Converter System in Java

---
+++ README.md
@@ -0,0 +1,11 @@
+# Numeric Converter System
+
+Simple algorithm that converts a value of a specific numeric system, made in java
+
+## Examples
+1. Converting from binary..
+    - convert(1101, 2) -> 13
+    - convert(10101, 2) -> 21
+2. Converting from hexadecimal
+    - convert("AF", 16) -> 31
+    - convert("FF", 16) -> 255
---
+++ java/NumericConverter.java
@@ -0,0 +1,64 @@
+public class NumericConverter {
+
+    // Numeric Converter, be sure to insert the right numeric base to precise conversion!
+
+    public static void main(String... args) {
+        int num = 101;
+        String hexadecimal = "FF";
+        System.out.println(convert(num, 2));
+        System.out.println(convert(hexadecimal, 16));
+    }
+
+    /*
+     *   Handles conversion with a long value as paramater
+     *   But only supports until decimal base
+     *   If the base is higher than 10, the result will not be right
+     */
+    public static long convert(long numToConvert, int base) {
+        String num = Long.toString(numToConvert);
+        long sum = 0;
+        
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int digit = Integer.parseInt(String.valueOf(num.charAt(i)));
+            sum += digit * Math.pow(base, num.length() - i - 1);
+        }
+        return sum;
+    }
+
+    // The same applies to this, but with an int value as paramater
+    public static int convert(int numToConvert, int base) {
+        String num = Long.toString(numToConvert);
+        int sum = 0;
+        
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int digit = Integer.parseInt(String.valueOf(num.charAt(i)));
+            sum += digit * Math.pow(base, num.length() - i - 1);
+        }
+        return sum;
+    }
+
+    /*
+     * Handles conversion with a String number parameter, also supports
+     * alphanumeric values, hexadecimal, for example.
+     * Supports until base 36, if base is higher than 36 it will not work as expected
+     */
+    public static long convert(String numToConvert, int base) {
+        if (numToConvert == null)
+            throw new IllegalArgumentException("num argument cannot be null");
+
+        String num = numToConvert.toUpperCase();
+        int sum = 0;
+        
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int digit = num.charAt(i);
+            if (Character.isAlphabetic(digit)) {
+                sum += (10 + digit - 'A') * Math.pow(base, num.length() - i -1);
+            }
+            else if (Character.isDigit(digit)) {
+                digit = Integer.parseInt(String.valueOf(num.charAt(i)));
+                sum += digit * Math.pow(base, num.length() - i - 1);
+            }
+        }
+        return sum;
+    }
+}
